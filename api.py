fea76241e6686e342adf0a75bd5ba1ddd90ffb4d
joqim <jo@joqim.com>
2023-06-07 15:05:31 -0600

    splitwise now uses oauth token and secret

--- api.py
+++ api.py
@@ -1,31 +1,201 @@
-from flask import Flask, jsonify, render_template, redirect, session, url_for, request
+from flask import Flask, jsonify, request, session, redirect
+from flask_cors import CORS
 from splitwise import Splitwise
 from flask_cors import cross_origin
 import json
 from splitwise.expense import Expense
 from splitwise.user import ExpenseUser
 import datetime
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 app.secret_key = "joqim"
 secret = ''
 
-@app.route("/")
+# Enable CORS for all routes
+CORS(app)
+
+load_dotenv()  # Load environment variables from .env file
+
+@app.route("/auth_code")
+def auth():
+    try:
+        print("Inside /auth_code in Flask", flush=True)
+        
+        consumer_key = os.getenv("CONSUMER_KEY")
+        consumer_secret = os.getenv("CONSUMER_SECRET")
+
+        sObj = Splitwise(consumer_key, consumer_secret)
+        url, secret = sObj.getAuthorizeURL()
+        
+        print("url", url, flush=True)
+        print("secret", secret, flush=True)
+        
+        # Store secret so you can retrieve it later
+        session['secret'] = secret
+        print("session", session)
+
+        res = {"redirect_url": url, "secret": secret}
+        return jsonify(res), 200
+
+    except Exception as e:
+        print("An error occurred in auth_code:", str(e), flush=True)
+        res = {"message": "error"}
+        return jsonify(res), 400
+
+@app.route("/access_token")
+def oauth_callback():
+    try:
+        print("inside /access_token - session")
+        oauth_token = request.args.get('oauth_token')
+        oauth_verifier = request.args.get('oauth_verifier')
+        secret = request.args.get('secret')
+
+        consumer_key = os.getenv("CONSUMER_KEY")
+        consumer_secret = os.getenv("CONSUMER_SECRET")
+        
+        sObj = Splitwise(consumer_key, consumer_secret)
+        session['secret'] = secret
+        
+        print("session", session, consumer_key, consumer_secret, flush=True)
+        access_token = sObj.getAccessToken(oauth_token, session['secret'], oauth_verifier)
+        
+        print("access token", access_token, flush=True)
+        session['access_token'] = access_token
+
+        # sObj.setAccessToken(session['access_token'])
+        # group = sObj.getGroup(28857816)
+        # print("group", group.getMembers()[0], flush=True)
+        
+        res = {"token": access_token}
+        return jsonify(res), 200
+
+    except Exception as e:
+        print("An error occurred in oauth_callback:", str(e), flush=True)
+        res = {"message": "error"}
+        return jsonify(res), 400
+
+@app.route('/groups', methods=['GET'])
+def groups():
+    try:
+        print("Inside /groups", request.args, flush=True)
+        
+        consumer_key = os.getenv("CONSUMER_KEY")
+        consumer_secret = os.getenv("CONSUMER_SECRET")
+        
+        oauth_token = request.args.get('oauth_token')
+        oauth_token_secret = request.args.get('oauth_token_secret')
+        
+        access_token = {
+            "oauth_token": oauth_token,
+            "oauth_token_secret": oauth_token_secret
+        }
+        
+        session['access_token'] = access_token
+        print("session", session, flush=True)
+        
+        sObj = Splitwise(consumer_key, consumer_secret)
+        sObj.setAccessToken(session['access_token'])
+        
+        groups = sObj.getGroups()
+
+        # Convert the groups to a JSON-serializable format
+        serialized_groups = []
+        for group in groups:
+            serialized_group = {
+                'id': group.id,
+                'name': group.name,
+                # Include any other required attributes
+            }
+            serialized_groups.append(serialized_group)
+
+        print("groups", serialized_groups, flush=True)
+        # Create the response dictionary
+        response = {"groups": serialized_groups}
+
+        # Return the response as JSON
+        return json.dumps(response), 200
+    
+    except Exception as e:
+        print("An error occurred:", str(e), flush=True)
+        res = {"message": "error"}
+        return jsonify(res), 400
+
+@app.route('/players', methods=['GET'])
+def players():
+    try:
+        print("Inside /players", request.args, flush=True)
+        
+        consumer_key = os.getenv("CONSUMER_KEY")
+        consumer_secret = os.getenv("CONSUMER_SECRET")
+        
+        oauth_token = request.args.get('oauth_token')
+        oauth_token_secret = request.args.get('oauth_token_secret')
+        groupId = request.args.get('group')
+        
+        access_token = {
+            "oauth_token": oauth_token,
+            "oauth_token_secret": oauth_token_secret
+        }
+        
+        session['access_token'] = access_token
+        print("session", session, flush=True)
+        
+        sObj = Splitwise(consumer_key, consumer_secret)
+        sObj.setAccessToken(session['access_token'])
+        group = sObj.getGroup(groupId)
+
+        # Convert the groups to a JSON-serializable format
+        serialized_players = []
+        for player in group.getMembers():
+            serialized_player = {
+                'id': player.id,
+                'email': player.email,
+                'first_name': player.first_name,
+                'last_name': player.last_name
+            }
+            serialized_players.append(serialized_player)
+
+        print("players", serialized_players, flush=True)
+        # Create the response dictionary
+        response = {"players": serialized_players}
+
+        # Return the response as JSON
+        return json.dumps(response), 200
+    
+    except Exception as e:
+        print("An error occurred:", str(e), flush=True)
+        res = {"message": "error"}
+        return jsonify(res), 400
+    
+@app.route("/update")
 def update_splitwise():
     try:
         print("inside /update in PYTHON SERVER", request.args, flush=True)
 
         group_id = request.args.get('group_id')
         total_money_paid = request.args.get('total_paid')
-
-        CONSUMER_KEY = request.args.get('CONSUMER_KEY')
-        CONSUMER_SECRET = request.args.get('CONSUMER_SECRET')
-        API_KEY = request.args.get('API_KEY')
+        
+        oauth_token = request.args.get('oauth_token')
+        oauth_token_secret = request.args.get('oauth_token_secret')
+                
+        access_token = {
+            "oauth_token": oauth_token,
+            "oauth_token_secret": oauth_token_secret
+        }
+        
+        session['access_token'] = access_token
+        print("session", session, flush=True)
+        
+        consumer_key = os.getenv("CONSUMER_KEY")
+        consumer_secret = os.getenv("CONSUMER_SECRET")
+        
+        sObj = Splitwise(consumer_key, consumer_secret)
+        sObj.setAccessToken(session['access_token'])
 
         players = request.args.getlist('players[]')
         parsed_players = [json.loads(player) for player in players]
-
-        sObj = Splitwise(CONSUMER_KEY, CONSUMER_SECRET, api_key=API_KEY)
 
         expense = Expense()
         expense.setCost(total_money_paid)
@@ -78,7 +248,6 @@ def get_ordinal_suffix(day):
             return "rd"
         else:
             return "th"
-
 
 @app.route("/test")
 def test():
